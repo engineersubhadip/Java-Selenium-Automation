8fcfb12e9e836a6a8f3f13b1939fa24efb268efc
Subhadip Das <subhadipdas911@gmail.com>
2024-11-17 20:48:08 +0530

    Slider Concept Implementation for FIltering out the Price

--- Selenium/src/test/java/Slider/SliderDemo.java
+++ Selenium/src/test/java/Slider/SliderDemo.java
@@ -16,10 +16,26 @@ public class SliderDemo {
 		WebElement minSlider = driver.findElement(By.xpath("//div[@id='slider-range']/span[1]"));
 		
 		int xMin = minSlider.getLocation().getX();
+		int yMin = minSlider.getLocation().getY();
 		
 		Actions a = new Actions(driver);
 		
-		a.dragAndDropBy(minSlider, xMin+100, 0).build().perform();
+		a.dragAndDropBy(minSlider, 100, yMin).build().perform();
+		
+		int currXMin = minSlider.getLocation().getX();
+		
+		
+		WebElement maxSlider = driver.findElement(By.xpath("//div[@id='slider-range']/span[2]"));
+		
+		int xMax = maxSlider.getLocation().getX();
+		int yMax = maxSlider.getLocation().getY();
+		
+		System.out.println("Max Slider X Cord : "+xMax);
+		a.dragAndDropBy(maxSlider, -350, yMax).build().perform();
+		
+		int currXMax = maxSlider.getLocation().getX();
+		
+		System.out.println("Max Slider current X Cord "+currXMax);
 	}
 
 }
