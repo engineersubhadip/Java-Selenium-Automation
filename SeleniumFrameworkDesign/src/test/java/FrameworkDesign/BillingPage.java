ace9d14a9cf7231b51ad57464c620a8915623c84
Subhadip Das <subhadipdas911@gmail.com>
2024-11-22 23:32:16 +0530

    Added Page Object Model Class for the Billing Page for verification of Product Purchase

---
+++ SeleniumFrameworkDesign/src/test/java/FrameworkDesign/BillingPage.java
@@ -0,0 +1,32 @@
+package FrameworkDesign;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import FrameworkDesign.AbstractComponents.AbstractComponent;
+
+public class BillingPage extends AbstractComponent{
+	
+	WebDriver driver;
+	
+	public BillingPage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	By confirmMessageBy = By.tagName("h1");
+	
+	@FindBy(tagName="h1")
+	WebElement confirmMessage;
+	
+	public String getConfirmMessage() {
+		waitForElementToAppear(confirmMessageBy);
+		
+		return confirmMessage.getText().toLowerCase();
+	}
+
+}
