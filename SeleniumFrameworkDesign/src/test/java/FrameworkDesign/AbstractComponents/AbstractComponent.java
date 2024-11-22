2003f34d56f1630d6b20b9e9b605b3b452fad03d
Subhadip Das <subhadipdas911@gmail.com>
2024-11-22 21:18:08 +0530

    Created Abstract Components for Reusable Code

---
+++ SeleniumFrameworkDesign/src/test/java/FrameworkDesign/AbstractComponents/AbstractComponent.java
@@ -0,0 +1,24 @@
+package FrameworkDesign.AbstractComponents;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractComponent {
+	
+	WebDriver driver;
+	
+	public AbstractComponent(WebDriver driver) {
+		this.driver = driver;
+	}
+//	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//section[@id='products']/div[@class='container']")));
+
+	public void waitForElementToAppear(By findBy) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(findBy));
+	}
+}
