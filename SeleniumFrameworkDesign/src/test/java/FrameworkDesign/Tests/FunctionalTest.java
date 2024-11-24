97dc1ef30e6476963d13d402556ebed23ff7f750
Subhadip Das <subhadipdas911@gmail.com>
2024-11-24 19:03:20 +0530

    Used HashMap to send the data in @DataProvider

--- SeleniumFrameworkDesign/src/test/java/FrameworkDesign/Tests/FunctionalTest.java
+++ SeleniumFrameworkDesign/src/test/java/FrameworkDesign/Tests/FunctionalTest.java
@@ -2,6 +2,7 @@ package FrameworkDesign.Tests;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -15,28 +16,26 @@ import FrameworkDesign.TestComponent.BaseTest;
 
 public class FunctionalTest extends BaseTest{
 
-
-//		String userPurchase = "iphone";
 		
-		@Test(dataProvider="data")
-		public void endToEndTest(String userEmail, String userPassword, String userPurchase) throws IOException, InterruptedException {
+		@Test(dataProvider="data", groups= {"purchase"})
+		public void endToEndTest(HashMap<String, String> data) throws IOException, InterruptedException {
 			
 //			Login Page :-
 			
-			ArrayList<Object> object = loginPage.loginApplication(userEmail, userPassword);
+			ArrayList<Object> object = loginPage.loginApplication(data.get("userEmail"), data.get("userPassword"));
 			
 			ProductCatalogue productcatalogue = (ProductCatalogue) object.get(0);
 			
 //			Product Catalogue Page :-
 			
-			CartVerification cartverification = productcatalogue.addProductToCart(userPurchase);
+			CartVerification cartverification = productcatalogue.addProductToCart(data.get("userPurchase"));
 			
 			Assert.assertEquals(productcatalogue.cartVerification(), "Product Added To Cart"); 
 		
 			
 //			Verification of Cart and Place Order :-
 			
-			Assert.assertTrue(cartverification.verifyCart(userPurchase));
+			Assert.assertTrue(cartverification.verifyCart(data.get("userPurchase")));
 			
 			cartverification.selectCountry("ind", "India");
 			
@@ -48,22 +47,35 @@ public class FunctionalTest extends BaseTest{
 		}
 
 		@Test(dependsOnMethods= {"endToEndTest"}, dataProvider="data")
-		public void findProductTest(String userEmail, String userPassword, String userPurchase) throws InterruptedException {
+		public void findProductTest(HashMap<String, String> data) throws InterruptedException {
 			
 //			Login Page :-
 			
-			ArrayList<Object> object =  loginPage.loginApplication(userEmail, userPassword);
+			ArrayList<Object> object =  loginPage.loginApplication(data.get("userEmail"), data.get("userPassword"));
 			
 			OrdersPage orderspage = (OrdersPage) object.get(1);
 			
 //			Orders Page :-
 			
-			Assert.assertTrue(orderspage.checkProduct(userPurchase));
+			Assert.assertTrue(orderspage.checkProduct(data.get("userPurchase")));
 		}
 		
 		
 		@DataProvider(name="data") 
 		public Object[][] getData() {
-			return new Object[][] {{"r1@abc.com","Test@1234","adidas"},{"shetty@gmail.com","Iamking@000","iphone"}};
+			
+			HashMap<String, String> hashMap1 = new HashMap<>();
+			
+			hashMap1.put("userEmail","r1@abc.com");
+			hashMap1.put("userPassword","Test@1234");
+			hashMap1.put("userPurchase","adidas");
+			
+			HashMap<String, String> hashMap2 = new HashMap<>();
+			
+			hashMap2.put("userEmail","shetty@gmail.com");
+			hashMap2.put("userPassword","Iamking@000");
+			hashMap2.put("userPurchase","iphone");
+			
+			return new Object[][] {{hashMap1},{hashMap2}};
 		}
 }
