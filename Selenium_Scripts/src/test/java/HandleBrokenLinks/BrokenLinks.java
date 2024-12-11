fad460cbf3030f19860e57c7756261ae667ed564
Subhadip Das <subhadipdas911@gmail.com>
2024-12-11 13:57:11 +0530

    Initial Commit of the Broken Link File

---
+++ Selenium_Scripts/src/test/java/HandleBrokenLinks/BrokenLinks.java
@@ -0,0 +1,34 @@
+package HandleBrokenLinks;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/*
+ * If we send a request to the server
+ * Based on the response code sent by the server, we will judge if it is a broken link or not
+ */
+
+public class BrokenLinks {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("http://www.deadlinkcity.com/");
+		wait.until(ExpectedConditions.titleContains("Dead Link City - The Museum of Dead Links"));
+		
+		WebElement brokenLink = driver.findElement(By.xpath("//a[contains(@href,404)]"));
+		
+		
+	}
+
+}
