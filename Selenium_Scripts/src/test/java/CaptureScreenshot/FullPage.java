60fa70eaba742ec63cd0f599af26fe7808f3d22b
Subhadip Das <subhadipdas911@gmail.com>
2024-12-12 10:25:14 +0530

    Captured the Full page screenshot

--- Selenium_Scripts/src/test/java/CaptureScreenshot/FullPage.java
+++ Selenium_Scripts/src/test/java/CaptureScreenshot/FullPage.java
@@ -1,10 +1,37 @@
 package CaptureScreenshot;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class FullPage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));	
+		driver.get("https://demo.nopcommerce.com/");
+		
+		wait.until(ExpectedConditions.titleContains("nopCommerce demo store"));
+		
+		File src =  ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(src, new File("K:\\AutomationScreenShots\\fullPage.png"));
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
 	}
 
 }
