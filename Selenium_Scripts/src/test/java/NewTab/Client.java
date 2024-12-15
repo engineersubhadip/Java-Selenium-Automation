8be90565ae2159a9192a12469d38881a11eabd2f
Subhadip Das <subhadipdas911@gmail.com>
2024-12-15 12:12:58 +0530

    How to open a new Tab and make driver switch there

---
+++ Selenium_Scripts/src/test/java/NewTab/Client.java
@@ -0,0 +1,22 @@
+package NewTab;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Client {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		
+//		1. Create a new Tab :-
+		
+		driver.switchTo().newWindow(WindowType.TAB); // by doing so, the browser will be switched and the control also will be there 
+		
+//		2. Hit the new URL :-
+		
+		driver.get("https://www.amazon.com/");
+	}
+
+}
