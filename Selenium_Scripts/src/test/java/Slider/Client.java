df10f24253e73f27fb8a5dee1a0d5a78fdc71ce2
Subhadip Das <subhadipdas911@gmail.com>
2024-12-15 10:35:10 +0530

    Implemented Slider Automation

---
+++ Selenium_Scripts/src/test/java/Slider/Client.java
@@ -0,0 +1,59 @@
+package Slider;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/*
+ * For Horizontal Slider, increase/decrease X-axis
+ * For vertical Slider, increase/decrease Y-axis
+ */
+
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.jqueryscript.net/demo/Price-Range-Slider-jQuery-UI/");
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		wait.until(ExpectedConditions.titleContains("jQuery UI Price Range Slider Demo"));
+		
+		WebElement leftSlider = driver.findElement(By.xpath("//div[@id='slider-range'] //span[1]"));
+		
+		WebElement rightSlider = driver.findElement(By.xpath("//div[@id='slider-range'] //span[2]"));
+		
+		int leftX =  leftSlider.getLocation().getX();
+		int leftY =  leftSlider.getLocation().getY();
+		
+		int rightX =  rightSlider.getLocation().getX();
+		int rightY =  rightSlider.getLocation().getY();
+		
+		System.out.println("LeftX -> "+leftX+" RightX -> "+rightX);
+		
+		Actions a = new Actions(driver);
+		
+		a.dragAndDropBy(leftSlider, 100, leftY).build().perform();
+		
+		a.dragAndDropBy(rightSlider, -200, rightY).build().perform();
+		
+		System.out.println(leftSlider.getLocation().getX());
+		
+		System.out.println(rightSlider.getLocation().getX());
+		
+	}
+
+}
