dd156df068c906e8df38b31043c93ff58e3abf26
Subhadip Das <subhadipdas911@gmail.com>
2024-12-18 11:23:15 +0530

    Happy Path for Dependency methods, if one @test fails/skips then the dependent methods will be skipped

---
+++ TestNG_Testing_Basics/src/test/java/DependencyMethods/HappyPath.java
@@ -0,0 +1,45 @@
+package DependencyMethods;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/*
+ * 1. Open App
+ * 2. Login
+ * 3. Search
+ * 4. Adv Search
+ * 5. Logout
+ * 
+ * dependsOnMethods -> Only works for @Test annotation. Meaning one @Test will be dependent on another @Test
+ * If one @Test fails/skips, then all the dependent Methods will also be skipped
+ */
+
+public class HappyPath {
+	
+	@Test(priority=1)
+	public void openApp() {
+		System.out.println("Opening the App");
+	}
+	
+	@Test(dependsOnMethods= {"openApp"},priority=2)
+	public void login() {
+		System.out.println("Logging in the App");
+		Assert.assertTrue(false);
+	}
+	
+	@Test(dependsOnMethods= {"login"},priority=3)
+	public void search() {
+		System.out.println("Searching in the App");
+	}
+	
+	@Test(dependsOnMethods= {"login"},priority=4)
+	public void advSearch() {
+		System.out.println("Adv Search in the App");
+	}
+	
+	@Test(dependsOnMethods= {"login"},priority=5)
+	public void logout() {
+		System.out.println("Logging out from the App");
+	}
+}
