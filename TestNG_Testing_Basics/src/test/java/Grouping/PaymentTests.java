5e68d133dbc9251ecd4834d7078adc2853b7bca3
Subhadip Das <subhadipdas911@gmail.com>
2024-12-18 12:41:37 +0530

    Executed both sanity AND regression test cases

--- TestNG_Testing_Basics/src/test/java/Grouping/PaymentTests.java
+++ TestNG_Testing_Basics/src/test/java/Grouping/PaymentTests.java
@@ -8,12 +8,12 @@ import org.testng.annotations.Test;
 
 public class PaymentTests {
 	
-	@Test(priority=1, groups= {"functional"})
+	@Test(priority=1, groups= {"sanity","regression","functional"})
 	public void paymentInRupees() {
 		System.out.println("Payment in rupees");
 	}
 	
-	@Test(priority=2, groups= {"functional"})
+	@Test(priority=2, groups= {"sanity","regression","functional"})
 	public void paymentInDollars() {
 		System.out.println("Payment in dollars");
 	}
