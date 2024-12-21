a6cf2bc225acaed03a0c8ae3e44f441b5814535b
Subhadip Das <subhadipdas911@gmail.com>
2024-12-21 22:48:08 +0530

    Created a Base Test which is storing the Driver SetUp and Other re-usable methods

--- OpenCart/src/test/java/testCases/TC001_AccountRegistrationTest.java
+++ OpenCart/src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -1,12 +1,6 @@
 package testCases;
 
-import java.time.Duration;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import pageObjects.AccountRegistrationPage;
