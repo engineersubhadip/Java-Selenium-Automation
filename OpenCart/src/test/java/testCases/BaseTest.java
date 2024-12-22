4218d40ec1b9048352c5e3161e3eff7a2a8775d1
Subhadip Das <subhadipdas911@gmail.com>
2024-12-22 11:18:48 +0530

    1. Added log4j2.xml file under resources and mentioned File type and captured Info level logs. 2. Loaded the log4j2.xml file inside the base test 3. added logs inside TC001_AccountRegistrationTest to capture the info logs and upon failure the error level logs

--- OpenCart/src/test/java/testCases/BaseTest.java
+++ OpenCart/src/test/java/testCases/BaseTest.java
@@ -2,51 +2,60 @@ package testCases;
 
 import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;  //Log4j
+import org.apache.logging.log4j.Logger;  //Log4j
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class BaseTest {
-	
+
 	public WebDriver driver;
-	
+	public Logger logger;
+
 	@BeforeClass
 	public void setUp() {
+
+		logger = LogManager.getLogger(this.getClass()); // this.getClass() -> will dynamically capture the current
+														// class(test case) we are running.
+		// line 21 will load the log4j2.xml file
+
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
+
 		driver.get("https://tutorialsninja.com/demo/");
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
 
 	public static String getRandomString() {
-		
+
 		String result = "";
-		for (int i=0; i<8; i++) {
-			int currASCII = 97 + (int)(Math.random() * (122-97 + 1));
-			char currChar = (char)currASCII;
+		for (int i = 0; i < 8; i++) {
+			int currASCII = 97 + (int) (Math.random() * (122 - 97 + 1));
+			char currChar = (char) currASCII;
 			result += currChar;
 		}
-		
+
 		return result;
-	}	
+	}
 
 	public static String getRandomNumberString() {
-		
+
 		String result = "";
-		for (int i=0; i<10; i++) {
-			int currASCII = 48 + (int)(Math.random() * (57-48 + 1));
-			char currChar = (char)currASCII;
+		for (int i = 0; i < 10; i++) {
+			int currASCII = 48 + (int) (Math.random() * (57 - 48 + 1));
+			char currChar = (char) currASCII;
 			result += currChar;
 		}
-		
+
 		return result;
 	}
 
@@ -54,5 +63,5 @@ public class BaseTest {
 		String result = getRandomString() + getRandomNumberString();
 		return result;
 	}
-	
+
 }
