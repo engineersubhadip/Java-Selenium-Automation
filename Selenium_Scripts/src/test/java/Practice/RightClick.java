6c024c265e12f1f2dabc8709e1c74cf7117c871c
Subhadip Das <subhadipdas911@gmail.com>
2025-01-18 13:14:48 +0530

    Series of Right Clicks

---
+++ Selenium_Scripts/src/test/java/Practice/RightClick.java
@@ -0,0 +1,36 @@
+package Practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RightClick {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().setSize(new Dimension(1440, 900));
+		driver.manage().window().maximize();
+		driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+		
+		WebElement target = driver.findElement(By.xpath("//span[contains(text(),'right click')]"));
+		WebElement target2 = driver.findElement(By.xpath("//li[contains(@class,'copy')]/span"));
+		
+		Actions a = new Actions(driver);
+		
+		a.moveToElement(target).contextClick().moveToElement(target2).contextClick().build().perform();
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+		Thread.sleep(2000);
+		
+		driver.quit();
+	}
+
+}
