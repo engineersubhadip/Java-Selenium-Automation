2215d974bb3d061db4f6d74574fdce8c61266102
Subhadip Das <subhadipdas911@gmail.com>
2025-01-19 10:31:57 +0530

    Located using ID, Name and performed Click Action on the DOM Level using JS Executor

---
+++ Selenium_Scripts/src/test/java/Locators/IDAndName.java
@@ -0,0 +1,28 @@
+package Locators;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IDAndName {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().setSize(new Dimension(1440,900));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");;
+		driver.findElement(By.id("inputUsername")).sendKeys("Rituparna");
+		driver.findElement(By.name("inputPassword")).sendKeys("Test@123");
+	
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//button[contains(@class,'signInBtn')]")));
+	}
+
+}
