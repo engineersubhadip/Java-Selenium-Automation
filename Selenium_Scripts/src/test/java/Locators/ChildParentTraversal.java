f111b3fe7925c7f6c5c547a302b6ba4efd1a84e9
Subhadip Das <subhadipdas911@gmail.com>
2025-01-19 11:03:33 +0530

    Made Sibling to Sibling traversal. Practice to Login

---
+++ Selenium_Scripts/src/test/java/Locators/ChildParentTraversal.java
@@ -0,0 +1,30 @@
+package Locators;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChildParentTraversal {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().setSize(new Dimension(1440,900));
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+//		Sibling to Sibling Traversal :-
+//		Travel from "Practice" to "Login"
+		
+		String result = driver.findElement((By.xpath("//header/div/button[1]/following-sibling::button[1]"))).getText();
+		System.out.println(result);
+	}
+
+}
