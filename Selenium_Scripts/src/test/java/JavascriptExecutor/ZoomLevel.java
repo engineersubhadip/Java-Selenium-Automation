4735a3351d8d11bc84551f03b96ff417ac8c019d
Subhadip Das <subhadipdas911@gmail.com>
2025-01-27 12:26:48 +0530

    How to set the Zoom level of the WebPage

--- Selenium_Scripts/src/test/java/JavascriptExecutor/ZoomLevel.java
+++ Selenium_Scripts/src/test/java/JavascriptExecutor/ZoomLevel.java
@@ -2,7 +2,7 @@ package JavascriptExecutor;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,10 +13,19 @@ public class ZoomLevel {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
 		
 //		How to minimize the Page :-
-		Thread.sleep(3000);
+		/*
+		Thread.sleep(5000);
 		driver.manage().window().minimize();
+		Thread.sleep(5000);
+		*/
+		driver.manage().window().maximize();
+		
+//		How to set the Zoom Level :-
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("document.body.style.zoom='50%'");
 	}
 
 }
