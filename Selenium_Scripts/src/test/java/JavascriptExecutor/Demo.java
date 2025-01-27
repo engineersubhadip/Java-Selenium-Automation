91fa30304a87806e8c5c320e605e10f27f4a79bf
Subhadip Das <subhadipdas911@gmail.com>
2025-01-27 11:34:20 +0530

    Send Keys alternative in Javascript Executor

---
+++ Selenium_Scripts/src/test/java/JavascriptExecutor/Demo.java
@@ -0,0 +1,32 @@
+package JavascriptExecutor;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().setSize(new Dimension(1440,900));
+		driver.manage().window().maximize();
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+//		SendKeys via JavascriptExecutor :-
+		
+		WebElement name = driver.findElement(By.xpath("//input[@id='name']"));
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		js.executeScript("arguments[0].setAttribute('value','Subhadip')", name);
+	}
+
+}
