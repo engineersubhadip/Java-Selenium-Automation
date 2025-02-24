2f63a66baea85dd5b6f72baf0aadcf9d71545a72
Subhadip Das <subhadipdas911@gmail.com>
2025-02-24 13:12:25 +0530

    How to locate Element in ever-changing dynamic web table

--- TestNG/src/test/java/DynamicTable/Client.java
+++ TestNG/src/test/java/DynamicTable/Client.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Client {
